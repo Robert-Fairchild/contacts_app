9b2c726fb996880140608ed65e0c23fd6cb3e160
Robert Fairchild <robertjoseph.fairchild@gmail.com>
2017-11-20 18:13:24 -0600

    add japan phone number

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,10 @@ class Contact < ApplicationRecord
   def full_name
     full_name = "#{first_name} #{last_name}"
   end 
+
+  def japan_phone_number
+    "+81 #{phone_number}"
+  end
   
   def as_json
     { 
@@ -12,6 +16,7 @@ class Contact < ApplicationRecord
       full_name: full_name,
       email: email,
       phone_number: phone_number,
+      japan_phone_number: japan_phone_number,
       created_at: created_at,
       updated_at: updated_at,
 
